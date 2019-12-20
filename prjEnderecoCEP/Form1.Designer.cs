95d6265a3bab81be2442f0b7d1be529021c78321
Jhonathan Araujo <jonathan.reboucas@unisys.com>
2019-12-20 10:05:57 -0300

    Consumindo Webservice

---
+++ prjEnderecoCEP/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace prjEnderecoCEP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.btnCEP = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.txtTipoLogradouro = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(77, 38);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(100, 20);
+            this.txtCEP.TabIndex = 0;
+            // 
+            // btnCEP
+            // 
+            this.btnCEP.Location = new System.Drawing.Point(222, 34);
+            this.btnCEP.Name = "btnCEP";
+            this.btnCEP.Size = new System.Drawing.Size(75, 23);
+            this.btnCEP.TabIndex = 1;
+            this.btnCEP.Text = "CEP";
+            this.btnCEP.UseVisualStyleBackColor = true;
+            this.btnCEP.Click += new System.EventHandler(this.btnCEP_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Endereço";
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Location = new System.Drawing.Point(140, 94);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.ReadOnly = true;
+            this.txtLogradouro.Size = new System.Drawing.Size(215, 20);
+            this.txtLogradouro.TabIndex = 3;
+            // 
+            // txtTipoLogradouro
+            // 
+            this.txtTipoLogradouro.Location = new System.Drawing.Point(77, 94);
+            this.txtTipoLogradouro.Name = "txtTipoLogradouro";
+            this.txtTipoLogradouro.ReadOnly = true;
+            this.txtTipoLogradouro.Size = new System.Drawing.Size(57, 20);
+            this.txtTipoLogradouro.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bairro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cidade";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(77, 136);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.ReadOnly = true;
+            this.txtBairro.Size = new System.Drawing.Size(195, 20);
+            this.txtBairro.TabIndex = 7;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(77, 177);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.ReadOnly = true;
+            this.txtCidade.Size = new System.Drawing.Size(195, 20);
+            this.txtCidade.TabIndex = 8;
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(298, 177);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.ReadOnly = true;
+            this.txtUF.Size = new System.Drawing.Size(57, 20);
+            this.txtUF.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCEP;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 225);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTipoLogradouro);
+            this.Controls.Add(this.txtLogradouro);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCEP);
+            this.Controls.Add(this.txtCEP);
+            this.Name = "Form1";
+            this.Text = "Buscador de CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Button btnCEP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.TextBox txtTipoLogradouro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtUF;
+    }
+}
+
---
+++ prjEnderecoCEP/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjEnderecoCEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCEP_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Alinha baixo cria um objeto de nome ds usando a classe DataSet
+                // DataSet usada para conexão de banco de dados 
+                DataSet ds = new DataSet();
+
+                // A linha abaixo cria uma variável de nome xml do tipo String, esta variável armazena a string de conexão do serviço web que iremos utilizar
+                // .Replace @cep -> substitui o que foi digitado na caixa de texto txtCEP pelo campo cep do servidor WEB 
+                // .Replace("-","") substitui o hífen por nada caso o cliente digite - no campo txtCEP 
+                String xml = "http://cep.republicavirtual.com.br/web_cep.php?cep=@cep&formato=xml".Replace("@cep",txtCEP.Text.Replace("-",""));
+
+                // A linha abaixo faz a leitura dos campos da tabela de CEP do servidor web 
+                ds.ReadXml(xml);
+
+                // As linhas abaixo preenchem os campos de acordo com oo dados recebidos (banco de dados de CEP free - "republica virtual")
+
+                txtTipoLogradouro.Text = ds.Tables[0].Rows[0]["tipo_logradouro"].ToString();
+
+                txtLogradouro.Text = ds.Tables[0].Rows[0]["logradouro"].ToString();
+
+                txtBairro.Text = ds.Tables[0].Rows[0]["bairro"].ToString();
+
+                txtCidade.Text = ds.Tables[0].Rows[0]["cidade"].ToString();
+
+                txtUF.Text = ds.Tables[0].Rows[0]["uf"].ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Serviço indisponível");
+            }
+        }
+    }
+}
